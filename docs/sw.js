cbf782398b7e506eda07d21b9f9ed0783c7cacc0
Carter Gividen <144549760+CarterG2099@users.noreply.github.com>
2026-08-10 11:49:22 -0600

    Stop animating the exercise figure.

--- docs/sw.js
+++ docs/sw.js
@@ -34,7 +34,7 @@
  * the whole strategy.
  */
 
-const CACHE_VERSION = 'plates-v49';
+const CACHE_VERSION = 'plates-v50';
 
 /** The shell. Everything needed to open the app and read local data. */
 const SHELL = [
